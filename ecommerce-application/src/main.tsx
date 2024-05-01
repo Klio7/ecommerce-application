daf7df713826dd2ed9ff9b37345f4d8ffa3400e4
kaliganoff <kaliganoff@yahoo.com>
2024-05-01 17:52:45 +0300

    fix: RSS-ECOMM-1_08 Add type assertion and fix scss import in main.tsx

--- ecommerce-application/src/main.tsx
+++ ecommerce-application/src/main.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import 'normalize.css'
-import './index.scss'
+import './styles.scss'
 import { ChakraProvider } from '@chakra-ui/react'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLDivElement).render(
   <React.StrictMode>
     <ChakraProvider>
       <App />
