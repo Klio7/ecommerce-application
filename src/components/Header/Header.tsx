a461c05cdcd0ee7e78ee7f028644b787bd825eed
kaliganoff <kaliganoff@yahoo.com>
2024-05-12 00:12:28 +0300

    feat: Add routing, header + almost empty Main and Not Found pages

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,31 @@
+import React from "react"
+import { Flex } from "@chakra-ui/react"
+import { Link } from "react-router-dom"
+
+function Header() {
+    return (
+        <Flex as='header' h="82px" alignItems="center" justifyContent="space-around">
+            <Flex align="center" gap="1em">
+                <img src="./src/assets/blyudo.png" alt="" />
+                Blyudo
+            </Flex> 
+            <Flex gap="1em">
+                <Link to='/'>Main</Link>
+                <Link to='/catalog'>Catalog</Link>
+                <Link to='/about'>About</Link>
+            </Flex>
+            <Flex gap="1em" alignItems="center">
+                <Link to='/profile'>
+                    <img src="./src/assets/Avatar.svg" alt="" />
+                </Link>
+                <Link to='/basket'>
+                    <img src="./src/assets/Shopping cart.svg" alt="" />
+                </Link>
+                <Link to='/signin'>Sign In</Link>
+                <Link to='/signup'>Sign Up</Link>
+            </Flex> 
+        </Flex>
+    )
+}
+
+export default Header;
