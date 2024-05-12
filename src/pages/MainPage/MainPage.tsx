0bd7ff22c2780e7a4abecba41f801da9638bf334
kaliganoff <kaliganoff@yahoo.com>
2024-05-12 23:29:00 +0300

    feat: Add a picture to Main Page

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,16 +1,28 @@
 import React from "react";
-import { Container, Flex } from "@chakra-ui/react";
+import { Box, Container, Flex, Image } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 
 function MainPage() {
   return (
-    <Container>
-      <Flex flexDirection="column">
-        <Link to="/">Main Page</Link>
-        <Link to="/signin">Sign In</Link>
-        <Link to="/signup">Sign Up</Link>
-      </Flex>
-    </Container>
+    <>
+      <Box position="relative">
+        <Image src="./src/assets/pic1.png" />
+        <Flex
+          paddingLeft="0.5em"
+          top="0px"
+          position="absolute"
+          w="100px"
+          h="100%"
+          bg="#826F66"
+          flexDirection="column"
+        >
+          <Link to="/">Main Page</Link>
+          <Link to="/signin">Sign In</Link>
+          <Link to="/signup">Sign Up</Link>
+        </Flex>{" "}
+      </Box>
+      <Container />
+    </>
   );
 }
 
