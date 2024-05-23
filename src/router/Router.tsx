4a5a98c7d94aeb536301c8fb351f06f6c11c6431
Anna Odintsova <Klio7@mail.ru>
2024-05-23 13:41:18 +0300

    feat: fetch name, description, images, price for the product

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -4,6 +4,7 @@ import MainPage from "../pages/MainPage/MainPage";
 import SignInPage from "../pages/SignInPage/SignInPage";
 import SignUpPage from "../pages/SignUpPage/SignUpPage";
 import NotFoundPage from "../pages/NotFoundPage/NotFoundPage";
+import DetailedProduct from "../components/DetailedProduct/DetailedProducts";
 
 function Router() {
   return (
@@ -12,6 +13,7 @@ function Router() {
       <Route path="/signin" element={<SignInPage />} />
       <Route path="/signup" element={<SignUpPage />} />
       <Route path="*" element={<NotFoundPage />} />
+      <Route path="/about" element={<DetailedProduct />} />
     </Routes>
   );
 }
